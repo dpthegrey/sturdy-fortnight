63bda07813feaddb8467dd6e84b5a996d1fe6080
dpthegrey <dpthegrey@gmail.com>
2021-07-08 16:03:45 +0530

    Lifecycle methods in Action

--- src/components/User.js
+++ src/components/User.js
@@ -2,13 +2,16 @@ import { Component } from "react";
 
 import classes from "./User.module.css";
 
-// Class based Component
 class User extends Component {
+  componentWillUnmount() {
+    console.log("User will unmount!");
+  }
+
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
   }
 }
-// Functional Component
+
 // const User = (props) => {
 //   return <li className={classes.user}>{props.name}</li>;
 // };
